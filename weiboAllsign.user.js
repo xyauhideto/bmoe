c929e3a2ff2cbe6b891c3ca2c9f096e45bb2b277
xyauhideto <xyauhideto@gmail.com>
2017-08-09 11:52:10 +0200

    Update links

--- weiboAllsign.user.js
+++ weiboAllsign.user.js
@@ -10,6 +10,8 @@
 // @grant          GM_deleteValue
 // @grant          GM_xmlhttpRequest
 // @version        2017.8.8.0
+// @downloadURL    https://greasyfork.org/scripts/32143-weiboallsign/code/weiboAllsign.user.js
+// @namespace https://greasyfork.org/users/32143
 // ==/UserScript==
 
 
